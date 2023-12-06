5b899604f69b3fafe830d68b3abe804e4eb81a1a
guigz <guillaume.breon@gmail.com>
2023-12-06 11:05:48 +0100

    class armor

--- game/gears/armor.py
+++ game/gears/armor.py
@@ -1,4 +1,15 @@
 class armor :
-    def __init__(self, name: str, defense: 50):
-        self.named = name
+    def __init__(self, name: str, defense: float = 25):
+        self.name = name
         self.defense = defense
+
+    def armor_type(self):
+        if self.name == 'armure partielle':
+            self.defense = 50
+            print (f"{self.name} choisie, vous avez {self.defense} points d'armure")
+        elif self.name == 'armure moyenne':
+            self.defense = 75
+            print (f"{self.name} choisie, vous avez {self.defense} points d'armure")
+        elif self.name == 'armure complète':
+            self.defense = 100
+            print (f"{self.name} choisie, vous avez {self.defense} points d'armure")
--- game/main.py
+++ game/main.py
@@ -1,24 +1,5 @@
-class Personnage:
-    def __init__(self, name: str, type_: str=None, HP:float=100, Shield:float=0, power:float=50):
-        self.name = name
-        self.HP = HP
-        self.Shield = Shield
-        self.power = power
-        self.type_ = type_
-    
-    def jeu(self):
-        while self.HP != 0:
+from gears.armor import armor
 
-            def attack(self, attack_type):
-                if self.power > 0:
-                    attack_type = input("Quelle attaque souhaitez vous faire ?")
-                    if attack_type == "Hit 1":
-                        print(f'{self.name} a donné un coup de poingts !\n')
-                        self.power -= 5
+armor = armor(input("Quelle armure souhaitez-vous équiper? (armure partielle, armure moyenne, armure complète) : ").lower())
 
-
-
-
-Bordane = Personnage("Bordane", "Chevalier")
-Bordane.jeu()
-
+armor.armor_type()
