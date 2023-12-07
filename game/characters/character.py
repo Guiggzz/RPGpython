53e6324ce11fca8a37f59a07353094d11b334e1c
guigz <guillaume.breon@gmail.com>
2023-12-07 09:28:17 +0100

    Update character.py

--- game/characters/character.py
+++ game/characters/character.py
@@ -1,2 +1,15 @@
-class Character:
+from gears.armor import armor
+from gears.weapon import weapon
+
+class Character(armor, weapon):
+    def __init__(self):
+        super().__init__()
+    def __init__(self, name: str, armor,  weapon: str = 'Le saint batôn de berger',  hp: float = 100 ):
+        self.name = name
+        self.armor = armor
+        self.weapon = weapon
+        self.hp = hp
+
+
+
     
