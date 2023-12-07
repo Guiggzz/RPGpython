7250edcc14b3d34bb64acb259e97b7940d8dd2f3
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2023-12-07 10:20:14 +0100

    tesst attaque

--- game/main.py
+++ game/main.py
@@ -4,14 +4,14 @@ from characters.character import Character
 
 little_armor = armor('armure partielle', 50)
 mid_armor = armor('armure moyenne', 75)
-complete_armor = armor('armure partielle', 100)
+complete_armor = armor('armure complète', 100)
 
 sword_weapon = Weapon('Epée', 30)
 pickaxe_weapon = Weapon('Pioche', 25)
 fist_weapon = Weapon('Coup de poing', 20)
 
 armor_choose = input("Quelle armure souhaitez-vous équiper 1, 2, 3 ? (1 : armure partielle, 2 : armure moyenne, 3 : armure complète) : ")
-user_armor = ''
+user_armor = None
 equipment_condition = False
 
 if armor_choose == "1":
@@ -24,36 +24,32 @@ elif armor_choose == "3":
     user_armor = complete_armor
     equipment_condition = True
 else: 
-    print("Vous n'aver pas choisi d'armure")
-
+    print("Vous n'avez pas choisi d'armure")
 
 weapon_choose = input("Quelle arme souhaitez-vous équiper 1, 2, 3 ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
-user_weapon = ''
+user_weapon = None
 
-if armor_choose == "1":
+if weapon_choose == "1":
     user_weapon = sword_weapon
     equipment_condition = True
-elif armor_choose == "2":
+elif weapon_choose == "2":
     user_weapon = pickaxe_weapon
     equipment_condition = True
-elif armor_choose == "3":
+elif weapon_choose == "3":
     user_weapon = fist_weapon
     equipment_condition = True
 else: 
-    print("Vous n'aver pas choisi d'armure")
+    print("Vous n'avez pas choisi d'arme")
 
-
-if equipment_condition == True:
-    print(f"Vous avez une {user_weapon.name} et elle met {user_weapon.damage} de points de dégat")
+if equipment_condition:
+    print(f"Vous avez une {user_weapon.name} et elle met {user_weapon.damage} de points de dégât")
     print(f"Vous avez une {user_armor.name} et elle a {user_armor.defense} de points de défense")
 
-
-jane = Character("Jane", armor_choose, weapon_choose, 120)
-john = Character("John", armor_choose, weapon_choose, 100)
+jane = Character("Jane", user_armor, user_weapon, 120)
+john = Character("John", user_armor, user_weapon, 100)
 
 john.attack(jane)
 print("Results: ")
 print(john.name, john.hp, "HP")
 print(jane.name, jane.hp, "HP")
 input("------------------")
-
