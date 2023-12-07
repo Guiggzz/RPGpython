dc91bbf3971be7661ae4fe67b0ffc86c5dccf620
guigz <guillaume.breon@gmail.com>
2023-12-07 12:02:09 +0100

    Update main.py

--- game/main.py
+++ game/main.py
@@ -2,6 +2,8 @@ from gears.armor import armor
 from gears.weapon import Weapon
 from characters.character import Character
 
+#implémentation des armes et armures
+no_armor = armor("Pas d'armure", 0)
 little_armor = armor('armure partielle', 50)
 mid_armor = armor('armure moyenne', 75)
 complete_armor = armor('armure complète', 100)
@@ -10,10 +12,16 @@ sword_weapon = Weapon('Epée', 30)
 pickaxe_weapon = Weapon('Pioche', 25)
 fist_weapon = Weapon('Coup de poing', 20)
 
+
+#Demande à l'utilisateurs ce qu'il veut comme équipement
 armor_choose = input("Quelle armure souhaitez-vous équiper 1, 2, 3 ? (1 : armure partielle, 2 : armure moyenne, 3 : armure complète) : ")
 user_armor = None
 equipment_condition = False
 
+weapon_choose = input("Quelle arme souhaitez-vous équiper 1, 2, 3 ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
+user_weapon = None
+
+#choix des armes
 if armor_choose == "1":
     user_armor = little_armor
     equipment_condition = True
@@ -25,9 +33,6 @@ elif armor_choose == "3":
     equipment_condition = True
 else: 
     print("Vous n'avez pas choisi d'armure")
-
-weapon_choose = input("Quelle arme souhaitez-vous équiper 1, 2, 3 ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
-user_weapon = None
 
 if weapon_choose == "1":
     user_weapon = sword_weapon
@@ -41,10 +46,16 @@ elif weapon_choose == "3":
 else: 
     print("Vous n'avez pas choisi d'arme")
 
+
+
+#print des équipements
 if equipment_condition:
     print(f"Vous avez une {user_weapon.name} et elle met {user_weapon.damage} de points de dégât")
     print(f"Vous avez une {user_armor.name} et elle a {user_armor.defense} de points de défense")
 
+
+
+#test
 jane = Character("Jane", user_armor, user_weapon, 120)
 john = Character("John", user_armor, user_weapon, 100)
 Barbare = Character("Barbare", user_armor, user_weapon, 100)
