26c5f526c7f076098f9bfb53209d9d581696bb21
guigz <guillaume.breon@gmail.com>
2023-12-07 15:53:20 +0100

    Update Barbarian.py

--- game/characters/Barbarian.py
+++ game/characters/Barbarian.py
@@ -1,12 +1,13 @@
 from gears.armor import armor
 from gears.weapon import Weapon
+from characters.character import Character
+
 class Barbarian:
     def __init__(self, name, armor: armor, weapon: Weapon = Weapon('Le saint batôn de berger'), hp: float = 80, character_type = None):
         self.name = name
         self.hp = hp
         self.character_type = character_type
-
-
+        
     def attack(self, other):
         for _ in range(2):
             Character.attack(self, other)
