79d44aa4eeee1cdac77e17484eba6c6b9cb8ac80
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2023-12-07 14:50:35 +0100

    tout les nlv modif

--- game/main.py
+++ game/main.py
@@ -12,45 +12,62 @@ sword_weapon = Weapon('Epée', 30)
 pickaxe_weapon = Weapon('Pioche', 25)
 fist_weapon = Weapon('Coup de poing', 20)
 
-def choose_equipment(character_name):
-    print(f"Équipement pour {character_name}:")
-    
-    # Armor selection
-    armor_choose = input(f"Quelle armure souhaitez-vous équiper pour {character_name} ? (1 : armure partielle, 2 : armure moyenne, 3 : armure complète) : ")
-    if armor_choose == "1":
-        user_armor = little_armor
-    elif armor_choose == "2":
-        user_armor = mid_armor
-    elif armor_choose == "3":
-        user_armor = complete_armor
-    else: 
-        print(f"Vous n'avez pas choisi d'armure pour {character_name}")
-        return None, None
-    
-    # Weapon selection
-    weapon_choose = input(f"Quelle arme souhaitez-vous équiper pour {character_name} ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
-    if weapon_choose == "1":
-        user_weapon = sword_weapon
-    elif weapon_choose == "2":
-        user_weapon = pickaxe_weapon
-    elif weapon_choose == "3":
-        user_weapon = fist_weapon
-    else: 
-        print(f"Vous n'avez pas choisi d'arme pour {character_name}")
-        return None, None
-    
-    return user_armor, user_weapon
+# Ask for Jane's equipment
+armor_choose_jane = input("Quelle armure souhaitez-vous équiper pour Jane ? (1 : armure partielle, 2 : armure moyenne, 3 : armure complète) : ")
+weapon_choose_jane = input("Quelle arme souhaitez-vous équiper pour Jane ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
+
+# Ask for John's equipment
+armor_choose_john = input("Quelle armure souhaitez-vous équiper pour John ? (1 : armure partielle, 2 : armure moyenne, 3 : armure complète) : ")
+weapon_choose_john = input("Quelle arme souhaitez-vous équiper pour John ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
 
 # Initialize equipment for Jane
-user_armor_jane, user_weapon_jane = choose_equipment("Jane")
+user_armor_jane = None
+user_weapon_jane = None
+
+if armor_choose_jane == "1":
+    user_armor_jane = little_armor
+elif armor_choose_jane == "2":
+    user_armor_jane = mid_armor
+elif armor_choose_jane == "3":
+    user_armor_jane = complete_armor
+else: 
+    print("Vous n'avez pas choisi d'armure pour Jane")
+
+if weapon_choose_jane == "1":
+    user_weapon_jane = sword_weapon
+elif weapon_choose_jane == "2":
+    user_weapon_jane = pickaxe_weapon
+elif weapon_choose_jane == "3":
+    user_weapon_jane = fist_weapon
+else: 
+    print("Vous n'avez pas choisi d'arme pour Jane")
+
+# Initialize equipment for John
+user_armor_john = None
+user_weapon_john = None
+
+if armor_choose_john == "1":
+    user_armor_john = little_armor
+elif armor_choose_john == "2":
+    user_armor_john = mid_armor
+elif armor_choose_john == "3":
+    user_armor_john = complete_armor
+else: 
+    print("Vous n'avez pas choisi d'armure pour John")
+
+if weapon_choose_john == "1":
+    user_weapon_john = sword_weapon
+elif weapon_choose_john == "2":
+    user_weapon_john = pickaxe_weapon
+elif weapon_choose_john == "3":
+    user_weapon_john = fist_weapon
+else: 
+    print("Vous n'avez pas choisi d'arme pour John")
 
 # Display equipment for Jane
 if user_armor_jane and user_weapon_jane:
     print(f"Jane a une {user_weapon_jane.name} qui met {user_weapon_jane.damage} de points de dégât")
     print(f"Jane a une {user_armor_jane.name} qui a {user_armor_jane.defense} de points de défense")
-
-# Initialize equipment for John
-user_armor_john, user_weapon_john = choose_equipment("John")
 
 # Display equipment for John
 if user_armor_john and user_weapon_john:
@@ -68,4 +85,4 @@ john.attack(jane)
 print("Results: ")
 print(john.name, john.hp, "HP et", john.armor.defense, "de defense")
 print(jane.name, jane.hp, "HP et", jane.armor.defense, "de defense")
-print("------------------") le sexe
+print("------------------")
