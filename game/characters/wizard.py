8d9e0e83a01e7112903e23e757d2de3ed8ad1bad
guigz <guillaume.breon@gmail.com>
2023-12-13 11:58:44 +0100

    wizard ett test etc

--- game/characters/wizard.py
+++ game/characters/wizard.py
@@ -3,12 +3,16 @@ from gears.weapon import Weapon
 from characters.character import Character
 
 class Wizard : 
-    def __init__(self, name, armor: armor = armor('Armure legere'), weapon: Weapon = Weapon('Le saint batôn de berger'), hp: float = 75, defense: float = 25):
+    def __init__(self, name, armor: armor = armor('Armure legere'), hp: float = 75, defense: float = 25, weapon: Weapon = Weapon('Le saint batôn de berger'), damage = 20):
         self.name = name
         self.armor = armor
         self.weapon = weapon
         self.hp = hp
         self.defense = defense
+        self.damage = damage
+
+    def attack(self, other):
+        Character.attack(self, other)
 
 class spell:
     def __init__(self,name, damage, mana_cost):
--- game/main.py
+++ game/main.py
@@ -12,55 +12,82 @@ sword_weapon = Weapon('Epée', 30)
 pickaxe_weapon = Weapon('Pioche', 25)
 fist_weapon = Weapon('Coup de poing', 20)
 
-weapon_choose = input("\nQuelle arme souhaitez-vous équiper 1, 2, 3 ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
-user_weapon = None
-
-if weapon_choose == "1":
-    user_weapon = sword_weapon
-    weapon_condition = True
-elif weapon_choose == "2":
-    user_weapon = pickaxe_weapon
-    weapon_condition = True
-elif weapon_choose == "3":
-    user_weapon = fist_weapon
-    weapon_condition = True
-else:
-    weapon_condition = False
-
-
-armor_choose = input("\nQuelle armure souhaitez-vous équiper 1, 2, 3 ? (1 : Petite armure, 2 : Moyenne armure, 3 : Grosse armure) : ")
-user_armor = None
-
-if armor_choose == "1":
-    user_armor = little_armor
-    armor_condition = True
-elif armor_choose == "2":
-    user_armor = mid_armor
-    armor_condition = True
-elif armor_choose == "3":
-    user_armor = complete_armor
-    armor_condition = True
-else: 
-    armor_condition = False
-
-type_choose = input("Choississez le type de votre personnage 1 : Barbarre ou 2 : Sorcier")
+type_choose = input("Choississez le type de votre personnage 1(Barbarre) ou 2(Sorcier): ")
 
 if type_choose == "1":
-     jane = Barbarian("Jane", user_armor, user_weapon, 120, user_armor.defense)
+    weapon_choose = input("\nQuelle arme souhaitez-vous équiper 1, 2, 3 ? (1 : Epée, 2 : Pioche, 3 : Coup de poing) : ")
+    user_weapon = None
+
+    if weapon_choose == "1":
+        user_weapon = sword_weapon
+        weapon_condition = True
+    elif weapon_choose == "2":
+        user_weapon = pickaxe_weapon
+        weapon_condition = True
+    elif weapon_choose == "3":
+        user_weapon = fist_weapon
+        weapon_condition = True
+    else:
+        weapon_condition = False
+
+    armor_choose = input("\nQuelle armure souhaitez-vous équiper 1, 2, 3 ? (1 : Petite armure, 2 : Moyenne armure, 3 : Grosse armure) : ")
+    user_armor = None
+
+    if armor_choose == "1":
+        user_armor = little_armor
+        armor_condition = True
+    elif armor_choose == "2":
+        user_armor = mid_armor
+        armor_condition = True
+    elif armor_choose == "3":
+        user_armor = complete_armor
+        armor_condition = True
+    else: 
+        armor_condition = False
+    jane = Barbarian("Jane", user_armor, user_weapon, 120, user_armor.defense)
+
 else:
-     jane = Wizard("Jane", user_armor, user_weapon, 120, user_armor.defense)
+    armor_choose = input("\nQuelle armure souhaitez-vous équiper 1, 2, 3 ? (1 : Petite armure, 2 : Moyenne armure, 3 : Grosse armure) : ")
+    user_armor = None
+
+    if armor_choose == "1":
+        user_armor = little_armor
+        armor_condition = True
+    elif armor_choose == "2":
+        user_armor = mid_armor
+        armor_condition = True
+    elif armor_choose == "3":
+        user_armor = complete_armor
+        armor_condition = True
+    else: 
+        armor_condition = False
+     
+    jane = Wizard("Jane", user_armor, 75, user_armor.defense)
+
 
 john = Character("John", mid_armor, pickaxe_weapon, 100, mid_armor.defense)
+
 attack_type = True
-
-if armor_condition and weapon_condition:
-        print(f'\nVous avez choisi : \n{user_weapon.name} pour attaquer et une {user_armor.name} qui à {user_armor.defense} de défense pour vous protéger')
-        print(f"\nVotre {user_weapon.name} met : \n{user_weapon.damage} de degat et l'{user_armor.name} de votre advairsaire à {john.defense} de defense")
-
-else:
+if type_choose == "1":
+    if armor_condition and weapon_condition:
+            print(f'\nVous avez choisi : \n{user_weapon.name} pour attaquer et une {user_armor.name} qui à {user_armor.defense} de défense pour vous protéger')
+            print(f"\nVotre {user_weapon.name} met : \n{user_weapon.damage} de degat et l'{user_armor.name} de votre advairsaire à {john.defense} de defense")
+    else:
         print("Vous n'avez pas choisi d'équipement valide")
+else:
+        if armor_condition : 
+            print(f'\nVous avez : \n un batôn qui met 20 de dégats et une {user_armor.name} qui à {user_armor.defense} de défense pour vous protéger')
+            print(f"\nVotre adversaire à {john.hp} de vie et à {john.defense} de defense")
+        else:
+            print("Vous n'avez pas choisi d'équipement valide")
+
+
 if isinstance(jane, Barbarian): 
     print("\nVous avez la classe Barbare. Vous tapez deux fois d'affilée!")
+
+
+
+    
 attack_type = True
 while attack_type == True:
     attack_value = input(f"\nVoulez vous attaquer votre enemie ? 1 : Oui, 2 : Non : ")
