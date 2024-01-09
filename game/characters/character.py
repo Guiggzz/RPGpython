03d8e5967e33c067a763a7b05c1a44ddfab769e5
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2024-01-09 16:01:54 +0100

    arena ask

--- game/characters/character.py
+++ game/characters/character.py
@@ -1,5 +1,6 @@
 from gears.armor import Armor
 from gears.weapon import Weapon
+from random import randint
 
 class Character:
     def __init__(self, name, armor=None, weapon=None, hp=100):
@@ -9,12 +10,26 @@ class Character:
         self.hp = hp
         self.defense = self.armor.defense
 
-    def attack(self, other):
-        if other.armor is not None:
-            if other.armor.defense >= self.weapon.damage:
-                other.armor.defense -= self.weapon.damage
+    def attack(self, other): 
+        random_value = randint(1, 100)
+        if random_value >= 5:
+            if other.armor is not None:
+                if other.armor.defense >= self.weapon.damage:
+                    other.armor.defense -= self.weapon.damage
+                else:
+                    other.hp -= self.weapon.damage - other.armor.defense
+                    other.armor.defense = 0
             else:
-                other.hp -= self.weapon.damage - other.armor.defense
-                other.armor.defense = 0
+                other.hp -= self.weapon.damage
+            if 90 <= random_value <= 100:
+                print("Vous allez mettre un coup critique qui met 2x plus de dégats")
+                if other.armor is not None:
+                    if other.armor.defense >= self.weapon.damage:
+                        other.armor.defense -= self.weapon.damage
+                    else:
+                        other.hp -= self.weapon.damage - other.armor.defense
+                        other.armor.defense = 0
+                else:
+                    other.hp -= self.weapon.damage
         else:
-            other.hp -= self.weapon.damage
+            print("L'attaque n'est pas parvenue à la cible...")
--- game/characters/wizard.py
+++ game/characters/wizard.py
@@ -1,40 +1,46 @@
 # from characters.character import Character
 from gears.spell import Spell
-
+from gears.armor import Armor
+from gears.weapon import Weapon
+from characters.character import Character
 class Wizard:
-    def __init__(self, name, armor, spell:Spell, hp=75, weapon=None, mana:float=100):
+    def __init__(self, name, armor, spell:Spell, hp=75, weapon='Saint Batons de berger', damage:float = 20, mana:float=100):
         self.name = name
         self.armor = armor
         self.weapon = weapon
+        self.damage = damage
         self.hp = hp
         self.spell = spell
         self.mana = mana 
     def attack(self, other):
-        
-        if self.mana >= self.spell.mana:
-            if other.armor is not None:
-                if other.armor.defense >= self.spell.damage:
-                    other.armor.defense -= self.spell.damage
-                    self.mana -= self.spell.mana
+        answer_user = input("'Y' pour lancer un sort, 'N' pour attaquer avec votre baton\n")
+        if answer_user == 'Y':
+            if self.mana >= self.spell.mana:
+                if other.armor is not None:
+                    if other.armor.defense >= self.spell.damage:
+                        other.armor.defense -= self.spell.damage
+                        self.mana -= self.spell.mana
+                    else:
+                        other.hp -= self.spell.damage - other.armor.defense
+                        other.armor.defense = 0
+                        self.mana -= self.spell.mana
                 else:
-                    other.hp -= self.spell.damage - other.armor.defense
-                    other.armor.defense = 0
-                    self.mana -= self.spell.mana
-            else:
-                other.hp -= self.spell.damage
-        else : 
-            self.mana += 15
-            if other.armor is not None:
-                if other.armor.defense >= self.weapon.damage:
-                    other.armor.defense -= self.weapon.damage
-                    print('Pas asser de mana, vous attaquer donc avec vos coup de poing')
+                    other.hp -= self.spell.damage
+            else : 
+                self.mana += 15
+                if other.armor is not None:
+                    if other.armor.defense >= self.weapon.damage:
+                        other.armor.defense -= self.weapon.damage
+                        print('Pas asser de mana, vous attaquer donc avec vos coup de poing')
+                    else:
+                        other.hp -= self.weapon.damage - other.armor.defense
+                        other.armor.defense = 0
+                        print('Pas asser de mana, vous attaquer donc avec vos coup de poing')
                 else:
-                    other.hp -= self.weapon.damage - other.armor.defense
-                    other.armor.defense = 0
-                    print('Pas asser de mana, vous attaquer donc avec vos coup de poing')
-            else:
-                print("test")
-                other.hp -= self.weapon.damage
+                    print("test")
+                    other.hp -= self.weapon.damage
+        else:
+            Character.attack(self, other)
 
     def print_mana(self):
         print(f'Vous avez {self.mana} mana')
