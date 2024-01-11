d717b5a2927d7e9d25aded0de31b97f7b54e9cfe
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2024-01-11 21:02:58 +0100

    correction armor

--- game/arena.py
+++ game/arena.py
@@ -32,8 +32,8 @@ class Arena:
     def print_mini_bilan(self):
         if self.first_character.hp >= 0 and self.second_character.hp >= 0:
             print("Mini-Bilan :\n")
-            print(f"{self.first_character.name} a {self.first_character.hp} HP restants et {self.first_character.armor} d'armure.")
-            print(f"{self.second_character.name} a {self.second_character.hp} HP restants et {self.second_character.armor} d'armure.")
+            print(f"{self.first_character.name} a {self.first_character.hp} HP restants et {self.first_character.armor.defense} d'armure.")
+            print(f"{self.second_character.name} a {self.second_character.hp} HP restants et {self.second_character.armor.defense} d'armure.")
             print("-----------------------------")
         elif self.first_character.hp <=0 :
             print("Mini-Bilan :\n")
